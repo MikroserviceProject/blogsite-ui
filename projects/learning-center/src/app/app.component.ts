90f1edea6f93e796b65f7fb1a88620dc773533dd
salihacicek <slihacicek@gmail.com>
2026-08-20 14:06:28 +0300

    feat: Add interactive terminal, fix animation jump, implement cascading collisions for nodes, setup learning-center project

---
+++ projects/learning-center/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MicroservicesLearningComponent } from './features/microservices-learning/microservices-learning.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [MicroservicesLearningComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'learning-center';
+}
